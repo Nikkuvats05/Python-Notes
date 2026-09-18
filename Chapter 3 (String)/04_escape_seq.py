2c2d301a6dca3c8a53a93318b8d054d82d62b247
Nikkuvats05 <nikhilvats208@gmail.com>
2026-09-18 15:00:32 +0530

    Escape sequence

---
+++ Chapter 3 (String)/04_escape_seq.py
@@ -0,0 +1,7 @@
+a = "Nikhil is a good boy \n but not a bad boy \t yeah \"offcource\""
+
+print (a)
+
+# \n = for new lihne
+# \t = for space as much tab
+# \"word\" = to print a word in single, double or tripkle quotes
