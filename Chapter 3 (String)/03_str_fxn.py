39ae2b56d2bb0dfb2bd6563d1a7b66bff7fe5282
Nikkuvats05 <nikhilvats208@gmail.com>
2026-09-18 15:00:13 +0530

    Functions of string

---
+++ Chapter 3 (String)/03_str_fxn.py
@@ -0,0 +1,14 @@
+name = "nikhil"
+
+s = "Radhe radhe Krishna"
+
+print ("len of ame is = ", len(name))
+
+print(s.upper())
+print(name.upper())
+
+print (s.title())
+print (name.capitalize())
+
+str = (s.replace("Radhe", "Shri"))
+print (str.title())
