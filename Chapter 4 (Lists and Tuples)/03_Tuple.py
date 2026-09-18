966042e49b94a876b2696f391f314be5a9fe5bb5
Nikkuvats05 <nikhilvats208@gmail.com>
2026-09-18 15:02:07 +0530

    Intro to Tuple

---
+++ Chapter 4 (Lists and Tuples)/03_Tuple.py
@@ -0,0 +1,5 @@
+a = ("asd", "cdf" ,"5", "3.12", "Rohan", True)
+print(a)
+
+a[0] = "ACF" # not workable because its immutable like string not like list its the main and only difference between tuple and list
+print(a)
