e9a479eb7e677d2ffb4c172d85566895d7278ee8
Nikkuvats05 <nikhilvats208@gmail.com>
2026-09-18 12:07:33 +0530

    String start

---
+++ Chapter 3 (String)/01_intro_to_String.py
@@ -0,0 +1,11 @@
+name = "Nikhil"
+# String can be written in single quotes, double and triple all are right and generally triple are used when multi line string
+name2 = 'Nikhil'
+name3 = '''Nikhil'''
+
+shortname = name[0:3]  #A new string that is from name string but start from 0th index to 2nd index not 3rd index
+char1 = name[1]  #It will give the 1st index char of name string
+
+print ("name = ", name)
+print ("shortname = ", shortname)
+print ("char1 = ", char1)
