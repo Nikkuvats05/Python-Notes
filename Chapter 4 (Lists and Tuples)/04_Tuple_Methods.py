bef79cd0758335babad589ae660a66358495bc2b
Nikkuvats05 <nikhilvats208@gmail.com>
2026-09-18 15:02:16 +0530

    Methods of Tuple

---
+++ Chapter 4 (Lists and Tuples)/04_Tuple_Methods.py
@@ -0,0 +1,10 @@
+a = ("asd", "cdf" ,5, 3.12, "Rohan", 5, True, 5)
+print(a)
+
+n = a.count(5) # how much time 5 is in tuple
+print(n)
+
+b= a.index(5) # only 1st occurance ka index batayega 
+print(b)
+
+print(len(a))
