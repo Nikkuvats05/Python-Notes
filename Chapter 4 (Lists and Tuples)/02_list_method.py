e31534a284730e363943b1ced9e1443ea1f5b1c9
Nikkuvats05 <nikhilvats208@gmail.com>
2026-09-18 15:01:47 +0530

    Methods in lists

---
+++ Chapter 4 (Lists and Tuples)/02_list_method.py
@@ -0,0 +1,22 @@
+friends = ["Apple", "Orange", "5" ,"3.12", "True", "Ram"]
+
+print(friends[0])
+
+friends.append("Nikhil")
+print(friends)
+
+list1 = [2,4,3,5,66,7]
+list1.sort()
+print(list1)
+
+list1.reverse()
+print(list1)
+
+list1.insert(3, 4444)
+print(list1)
+
+list1.pop(2)
+print(list1)
+
+list1.remove(4444)
+print(list1) 
