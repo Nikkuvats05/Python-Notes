f14cd3e87e30b9fb71f66569f065366b7ce819dd
Nikkuvats05 <nikhilvats208@gmail.com>
2026-09-18 15:01:34 +0530

    Intro to list

---
+++ Chapter 4 (Lists and Tuples)/01_List.py
@@ -0,0 +1,8 @@
+friends = ["Apple", "Orange", "5" ,"3.12", "True", "Ram"]
+
+print(friends[0])
+
+friends[0]="Banana" # This we cannot do in strings means we cannot change strings
+print (friends[0])
+
+print(friends[0:4])
