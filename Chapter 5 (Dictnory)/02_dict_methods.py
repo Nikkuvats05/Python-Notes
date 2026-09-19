569566e4ffffaaca9f289184f71378410363ba61
Nikkuvats05 <nikhilvats208@gmail.com>
2026-09-19 23:45:42 +0530

    dict methods

---
+++ Chapter 5 (Dictnory)/02_dict_methods.py
@@ -0,0 +1,16 @@
+marks = { 
+    "Nikhil": 100,  
+   "Subham" : 56,
+   "Rohan" : 50,
+   90 : "Mohan"
+}
+
+print(marks.items()) 
+
+print(marks.keys())
+
+print(marks.values())
+
+marks.update({"Nikhil" : 99, "Radhika": 100})
+
+print(marks)
