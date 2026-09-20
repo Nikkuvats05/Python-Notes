cfb745f10bfb5c26dad52e028bd2dfa6e193fbba
Nikkuvats05 <nikhilvats208@gmail.com>
2026-09-20 10:28:42 +0530

    Practice on dictionary and sets

---
+++ Chapter 5 (Dictnory)/practice_01.py
@@ -0,0 +1,7 @@
+# Write a program to create a dictionary of Hindi words with values as their English translation. Provide user with an option to look it up!
+
+words ={"madad": "help", "khana": "food", "padhai": "study", "billi": "cat"}
+
+word = input("Enter the word you want to know english meaning = ")
+
+print(words[word])
