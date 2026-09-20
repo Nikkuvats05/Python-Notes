1e49dc8162b239bca1c4555ec3de56a2a2465a4e
Nikkuvats05 <nikhilvats208@gmail.com>
2026-09-20 10:46:33 +0530

    if elif else ladder

---
+++ Chapter 6 (Conditional Expressions)/02_if_elif_else_ladder.py
@@ -0,0 +1,15 @@
+a = int(input("Enter your age = "))
+
+if(a>=18):
+    print("You are eligible")
+
+elif(a<0):
+    print("Enter a valid age")
+
+elif(a==0):
+    print("You entered 0 thats not acceptable")
+
+else:
+    print("Sorry, you are below age")
+
+print("End of letter")    
