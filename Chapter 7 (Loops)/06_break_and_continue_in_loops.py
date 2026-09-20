d31431ca915fcc6bedd9eccb66688c7bd7ffe4c5
Nikkuvats05 <nikhilvats208@gmail.com>
2026-09-20 18:31:36 +0530

    break and continue in loops

---
+++ Chapter 7 (Loops)/06_break_and_continue_in_loops.py
@@ -0,0 +1,12 @@
+for i in range(1,100):
+    if(i==34):
+        break
+    print(i)
+
+for i in range(1,16):
+    if(i==10):
+        continue
+    print(i)
+
+for i in range (1,15):
+    pass               ########## It will do nothing because of pass keyword
