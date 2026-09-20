90b8b864183261eded773ea2f5ea1d88544f9e9e
Nikkuvats05 <nikhilvats208@gmail.com>
2026-09-20 18:19:40 +0530

    loops in list

---
+++ Chapter 7 (Loops)/03_list_using_while.py
@@ -0,0 +1,6 @@
+l = ["Nikhil", 123, "Mohan", True, "Subham"]
+
+i = 0
+while(i<len(l)):
+    print(l[i])
+    i=i+1
---
+++ Chapter 7 (Loops)/04_for_loop_in_list.py
@@ -0,0 +1,14 @@
+l = ["Nikhil", 123, "Mohan", True, "Subham"]
+
+for i in l:
+    print(i)
+
+t = (1,2,3,4,5,6)
+
+for i in t:
+    print(i)
+
+s = "Nikhil"
+
+for i in s:
+    print(i)
