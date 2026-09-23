dc5d9d4143b6eaf2c5d8518ffc03ead428942574
Nikkuvats05 <nikhilvats208@gmail.com>
2026-09-23 12:37:21 +0530

    Intro to Function

---
+++ Chapter 8 (Function)/01_Functions.py
@@ -0,0 +1,9 @@
+def avg(a,b,c):
+    ans = (a+b+c)/3
+    return ans
+
+a = int(input("enter the 1st no = "))
+b = int(input("enter the 2nd no = "))
+c = int(input("enter the 3rd no = "))
+
+print(avg(a,b,c))
---
+++ Chapter 8 (Function)/02_fxn_with_argument.py
@@ -0,0 +1,7 @@
+def goodday(name, ending):
+    print(f"good morning {name} , {ending}")
+
+name = input("Enter the name = ")
+goodday(name, "Thank you")
+
+goodday("Mohan", "Thanks")
