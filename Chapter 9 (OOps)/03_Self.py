a0232bd24bce1077040cbe3107eca1f7516d940f
Nikkuvats05 <nikhilvats208@gmail.com>
2026-09-26 10:44:31 +0530

    Static method

--- Chapter 9 (OOps)/03_Self.py
+++ Chapter 9 (OOps)/03_Self.py
@@ -5,6 +5,7 @@ class employee():
     def getinfo(self):
         print(f"lang is {self.lang} and salery is {self.salery}")
 
+    @staticmethod #Agar koi fxn static h to it means it doesnot need any object and we can see also it only print string donot need any data from any object.
     def greet(self):
         print("Good morning")
    
